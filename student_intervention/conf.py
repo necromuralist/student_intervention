02e215aec03c6b40885e5c183979082e442ec3ac
rsnakamura <rsnakamura@acm.org>
2016-03-19 18:31:34 -0700

    Updated description of Logistic Regression

--- student_intervention/conf.py
+++ student_intervention/conf.py
@@ -59,7 +59,7 @@ copyright = u'2016, Russell Nakamura'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2'
+version = '0.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
